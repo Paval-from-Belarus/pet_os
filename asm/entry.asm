8d0e0b1fc39b6c6f4a4025a67e411d9bc35147c5
Paval-from-Belarus <star-war-sandakan@mail.ru>
2024-08-23 15:06:10 +0300

    save changes

---
+++ asm/entry.asm
@@ -0,0 +1,386 @@
+format Elf
+;The entry point to kernel
+DOS_SEG = 0x0
+DOS_OFFSET = 0x600
+include 'PetOS/linker.inc'
+include 'PetOS/format.inc'
+insert  'Services/Services.h'
+insert  'FileSystem/FileSystem.h'
+insert  'System/Executer/Executer.h'
+insert  'System/Memory/PagingGL.asm'
+insert  'System/BIOS/BiosUtilsGL.asm'
+insert  'System/Kernel/KernelGL.asm'
+insert  'Text/Text.h'
+insert  'Drivers/Types.h'
+insert  'macro/proc32.inc'
+insert  'Boot/Boot.h'
+BREAKPOINT_START_OFFSET equ 0x600 ;DOS_OFFSET but for preproc
+ENTRY_OFFSET = IO_ADDR ;OS loader will load kernel on this address
+;The kernel physically is stored at LOAD_KERNEL_ADDRESS (or LOAD_KERNEL_SEG:0h)
+;last program was invoked was looader
+;it had saved in dl disk driver index to interruct with DISK via BIOS
+section '.init_text' executable
+;@Declare{enum=KernelStack}
+extrn 'KERNEL_STACK_SIZE' as KernelStack.size
+;@Declare{enum=Kernel}
+extrn 'KERNEL_SIZE' as Kernel.size
+extrn 'main' as Kernel.run
+extrn KERNEL_VIRTUAL_OFFSET
+extrn KERNEL_PHYSICAL_OFFSET
+;the offset in memory where the end of kernel is supposed to be
+;probably use only KERNEL_SIZE... IT's enought to align memory
+;copy kernel to highert (0x10_000) -> add KERNEL_SIZE and init some essential structures (such as PageDirectory and other)
+;Pass new heap offset (the limit in memory ) -> this's without any sense. Because 
+public MAX_KERNEL_SIZE
+public AccessPoint as '_start'
+AccessPoint: ;point where execution is passes after loading
+use16
+org DOS_OFFSET
+jmp EntryPoint
+Kernel.loaderProperties LoaderProperties
+Kernel.pagingProperties PagingProperties
+insert 'System/Kernel/LegacyBoot.asm' ;some routines for legacy boot
+EntryPoint:
+     mov ax, DOS_SEG
+     push ax ax ax
+     pop es ds ss
+     mov sp, DOS_STACK_OFFSET
+     call Kernel.checkHardware
+     jc .stopExecution
+     mov bx, Kernel.loaderProperties
+     mov byte [ds: bx + LoaderProperties.bBootDevice], dl ;from BIOS-native loader
+     mov ax, Kernel.loaderProperties + LoaderProperties.rangeList
+     call Kernel.collectPhysicalMemory
+     ;mov ax, ax
+     call Kernel.checkMemory ;all debug info was already printed
+     jc .stopExecution
+     mov eax, GlobalDescriptorTable.handle
+     call Kernel.setProtectedMode
+.stopExecution:
+     hlt ;kernel cannot loaded
+use32
+LoadingPoint:
+     call Kernel.setPaging
+;Input:
+;None
+;Output:
+;None
+;Notes:
+;Loader should be already in protected mode
+;This method should be replaced by disk interraction
+;
+proc Kernel.copy uses esi edi
+     mov esi, ENTRY_OFFSET
+     mov edi, KERNEL_PHYSICAL_OFFSET
+     mov ecx, Kernel.size
+     cld
+     rep movsb ;but kernel should be paged aligned  
+ret
+endp
+;Input:
+;None
+;Output:
+;if cf clear -> all is done
+;if cf set -> paging error (system crash)
+;
+Kernel.setPaging:
+     mov eax, Kernel.loaderProperties + LoaderProperties.rangeList
+     mov edx, Kernel.pagingProperties + PagingProperties.captureRecList
+     call extractPivots
+.acquireKernelSize:
+     mov ebx, edx ;save captureRecList
+     mov eax, Kernel.size
+     add eax, PageSize.bytes - 1
+     cdq
+     mov ecx, PageSize.bytes
+     div ecx
+     push eax ;save the whole number of pages for kernel
+     mov edx, eax 
+     mov eax, ebx
+     call captureMemory ;only acquire memory for kernel code
+     jc .exit
+     cmp edx, KERNEL_PHYSICAL_OFFSET
+     jne .exit
+.initPaging:
+     ;mov eax, eax
+     mov edx, 1 ;single page for directory
+     call captureMemory
+     jc .exit
+     mov ebx, edx ;save directory offset
+     ;mov eax, eax -> holds CaptureRangeRec
+     mov edx, PageDirectory.ENTRIES_CNT
+     call captureMemory
+     jc .exit ;failed to allocated enought memory for PageTables -> it's impossible, but let make check
+     mov ebp, edx ;save page tables in ebp
+     mov eax, ebx ;restore directore offset
+     ;mov edx, edx
+     call MMU.init
+.configureKernel:
+     pop ecx ;restore the count of pages for kernel
+     add ecx, (KERNEL_LOW_ADDRESSES_SIZE + PageSize.bytes - 1) / PageSize.bytes ;the main idea to use lower addresses in kernel is to use V8086
+     mov eax, PageDirEntry.Present or PageDirEntry.Writable
+     mov edx, PageTableEntry.Present or PageTableEntry.Writable
+     ;mov ebx, ebx
+     xor esi, esi
+     mov edi, esi ;identity mapping
+     pusha ;save all register because they will be used to map the kernel in higher addresses
+     call MMU.markRegion
+     popa
+     mov edi, KERNEL_VIRTUAL_OFFSET ;map to higher addresses
+     call MMU.markRegion
+     ;map PageDirectory
+     add edi, ebx ;no needs to substract KERNEL_PHYSICAL_OFFSET from edi because virtual mapping accept low addresses as a part of kernel
+     ;add edi, ebx - KERNEL_PHYSICAL_OFFSET + KERNEL_LOW_ADDRESSES_SIZE
+     mov esi, ebx
+     mov eax, PageDirEntry.Present or PageDirEntry.Writable
+     mov edx, PageTableEntry.Present or PageTableEntry.Writable
+     mov ecx, 1 ;PageDirectory occure only 1 page
+     ;mov ebx, ebx
+     call MMU.markRegion
+     add edi, PageSize.bytes
+     mov esi, ebp ;tricky way to acquire existing value
+     todo 'replace addresing kernel only to read'
+     mov eax, PageDirEntry.Present or PageDirEntry.Writable
+     mov edx, PageTableEntry.Present or PageTableEntry.Writable
+     mov ecx, PageDirectory.ENTRIES_CNT
+     ;mov ebx, ebx
+     call MMU.markRegion
+     add edi, PageDirectory.ENTRIES_CNT * PageSize.bytes ;next virtual address
+.createInterruptStack:
+     mov eax, KernelStack.size
+     add eax, PageSize.bytes - 1
+     mov ecx, PageSize.bytes
+     cdq
+     div ecx
+     mov edx, eax ;count of pages for stack
+     push eax ;save count of pages
+     mov eax, Kernel.pagingProperties + PagingProperties.captureRecList
+     call captureMemory
+     pop ecx ;quickly restore page count
+     jc .exit ;impossible to capture stack memory; year stack is not clear, but hlt never return...
+     ;mov edi, edi
+     mov esi, edx
+     mov eax, PageDirEntry.Present or PageDirEntry.Writable
+     mov edx, PageTableEntry.Present or PageTableEntry.Writable
+     ;mov ebx, ebx
+     push ecx
+     call MMU.markRegion
+     pop ecx ;restore page count
+     ;now all prepositions for paging are done; let's copy kernel to hight addresse
+.configureProperties:
+     imul ecx, PageSize.bytes
+     add edi, ecx ;heap offset
+     ;consider to check each stask access
+     mov edx, Kernel.pagingProperties
+     mov dword [edx + PagingProperties.lpHeap], edi
+     mov dword [edx + PagingProperties.lpPageDirectory], ebx
+     add dword [edx + PagingProperties.lpPageDirectory], KERNEL_VIRTUAL_OFFSET
+     mov eax, Kernel.pagingProperties + PagingProperties.captureRecList + CaptureRecList.records - ENTRY_OFFSET
+     call Kernel.toKernelVirtualAddress
+     mov dword [edx + PagingProperties.lpCaptureRec], eax
+     mov eax, GlobalDescriptorTable.handle - ENTRY_OFFSET
+     call Kernel.toKernelVirtualAddress
+     mov dword [edx + PagingProperties.lpGDTHandle], eax
+     call Kernel.copy
+     mov eax, Kernel.pagingProperties - ENTRY_OFFSET
+     call Kernel.toKernelVirtualAddress
+     mov ecx, eax
+     mov eax, ebx
+     mov edx, edi ;stack offset is the same as heap, but grows in forward direction
+     call Kernel.switchPaging
+.exit:
+hlt ;never return
+;Input:
+;eax -> physical offset
+;Output:
+;eax -> virtual offset
+;all registers are saved
+;Notes:
+;add to value KERNEL_VIRTUAL_OFFSET and KERNEL_PHYSICAL_OFFSET 
+;
+Kernel.toKernelVirtualAddress:
+push edx
+     mov edx, KERNEL_VIRTUAL_OFFSET
+     add edx, KERNEL_PHYSICAL_OFFSET
+     add eax, edx
+pop edx
+ret
+
+;Input:
+;al -> value
+;ecx -> count of value to be filled
+;es:edx -> offset where values should be copied
+;Output:
+;None
+;
+Memory.fillBytes:
+push edi
+     mov edi, edx
+     cld
+     rep stosb    
+pop edi
+ret
+;Input:
+;eax -> value
+;ecx -> count of values to be filled
+;es:edx -> offset where values should be copied
+;Output:
+;None
+;
+Memory.fillDwords:
+push edi
+     mov edi, edx
+     cld
+     rep stosd
+pop edi
+ret
+;Input:
+;ax -> value
+;ecx -> count of values to be filled
+;es:edx -> offset where values should be copied
+;Output:
+;None
+;
+Memory.fillWords:
+push edi
+     mov edi, edx
+     cld
+     rep stosw
+pop edi
+ret
+insert 'System/Kernel/MMU.asm'
+;Input:
+;ds:eax -> addresss of source `RangeRecList` used to build pivot's list
+;es:edx -> address of buffer to store CaptureRecList
+;Output:
+;ds:eax -> saved
+;es:edx -> saved
+;
+proc extractPivots uses esi edi
+     local dPivotCnt : DWORD
+     mov esi, eax
+     mov edi, edx
+     movzx ecx, byte [eax + RangeRecList.bRecCnt]
+     mov dword [dPivotCnt], ecx
+     mov byte [es: edx + CaptureRecList.dRecCnt], 0
+     add eax, RangeRecList.records
+     add edx, CaptureRecList.records
+.mapLoop:
+     cmp dword [dPivotCnt], 0
+     jz .exit
+     cmp dword [eax + MemRangeRec.dOffset], 0
+     jz .nextRec 
+.mapRec:
+     inc dword [es: edi + CaptureRecList.dRecCnt]
+     push eax edx
+     mov ecx, dword [eax + MemRangeRec.dSize]
+     ; sub ecx, dword [eax + MemRangeRec.dOffset]
+     ; inc ecx
+     mov eax, PageSize.bytes
+     xchg ecx, eax
+     cdq ;eax => edx:eax
+     div ecx
+     xchg ecx, eax ;ecx holds integer count of page in MemZone
+     pop edx eax
+     mov dword [es: edx + CaptureRangeRec.dNextPage], 0
+     mov dword [es: edx + CaptureRangeRec.dPageCnt], ecx
+     push dword [eax + MemRangeRec.dOffset]
+     pop dword [es: edx + CaptureRangeRec.dMemOffset]
+     add edx, sizeof.CaptureRangeRec
+.nextRec:
+     add eax, sizeof.MemRangeRec
+     dec dword [dPivotCnt]
+     jmp .mapLoop 
+.exit:
+     mov eax, esi
+     mov edx, edi
+ret
+endp
+
+;Input:
+;es:eax -> CaptureRecList
+;edx -> page count to be captured
+;Output:
+;es:eax -> `CaptureRecList` (saved in any case)
+;if cf clear -> `edx` holds physical memory offset free to write
+;if cf set -> requested page cnt is not available
+;
+proc captureMemory uses ebx eax 
+     movzx ecx, byte [es: eax + CaptureRecList.dRecCnt]
+     add eax, CaptureRecList.records
+     mov ebx, eax
+.searchLoop:
+     mov eax, dword [es: ebx + CaptureRangeRec.dPageCnt]
+     sub eax, dword [es: ebx + CaptureRangeRec.dNextPage]
+     jle .nextRec
+     cmp eax, edx ;edx holds capture cnt
+     jae .foundRec
+.nextRec:
+     add ebx, sizeof.CaptureRangeRec
+     loop .searchLoop
+.notFound:
+     stc
+     jmp .exit
+.foundRec: ;ebx holds Rec
+     mov eax, dword [es: ebx + CaptureRangeRec.dNextPage]
+     imul eax, PageSize.bytes ;relative offset
+     add dword [es: ebx + CaptureRangeRec.dNextPage], edx ;add capture cnt
+     add eax, dword [es: ebx + CaptureRangeRec.dMemOffset]
+     mov edx, eax
+     clc
+.exit:
+ret
+endp
+
+;@Declare{struct=PageManager}
+;@Description {By this, to init PageManager is obligator only pass pointer to PageDirectory (or offset) Notes about OS: from the beginning, Operation system works without paging!}
+struct PageManager 
+{
+        pPageDirectory dd ? ;page_directory holds pointers to PageTable -> so it's excessive to holds additional pointers
+}
+ends
+;Input:
+;eax -> physical offset
+;edx -> virtual_offset
+;Output:
+;eax -> pointer in PageTable where entry is storing
+;Notes:
+;PageDirectory holds entry that marked as present (page directory is present)
+;PageManager allocate memory only for PageTable
+;Because pages should be addressed to, new allocated PageTable automatically marked
+;Notes:
+;To prevent excessive using of memory, it's highly recommended to pass only page-aligned physical offset
+;
+PageManager.markPage:
+
+
+ret
+;Input:
+;eax -> virtual_address
+;Output:
+;if cf is clear - all is done
+;if cf is set -> something goes wrong
+;
+PageManager.freePage:
+
+ret
+@@IDT String[$ mod 8]
+;@Declare{struct=IDTEntry}
+struct IDTEntry 
+{
+     .wLowOffset  dw ?
+     .wSegment    dw ?
+     .wFlags      dw ?
+     .wHighOffset dw ?
+}
+ends
+
+align 4
+GlobalDescriptorTable:   
+.null: GDTEntry.nullEntry
+;dos entries
+.kernelCode: GDTEntry.valueOf (base: 0, limit: 0xFFFFF, type: CodeSelector.Readable, level: 00b, present: 1, attr: SEG_ATTR_32_BIT, granularity: 1b)
+.kernelData: GDTEntry.valueOf (base: 0, limit: 0xFFFFF, type: DataSelector.Writable, level: 00b, present: 1, attr: SEG_ATTR_32_BIT, granularity: 1b)
+@@:
+GlobalDescriptorTable.handle:
+GDTHandle.valueOf @B - GlobalDescriptorTable, GlobalDescriptorTable
--- kernel/src/pet_os.rs
+++ kernel/src/pet_os.rs
@@ -7,12 +7,10 @@
 #![feature(const_trait_impl)]
 #![feature(abi_x86_interrupt)]
 #![feature(allocator_api)]
-
 #![feature(pointer_byte_offsets)]
 #![feature(ptr_sub_ptr)]
 #![feature(offset_of)]
 #![feature(ascii_char)]
-
 // really raw features
 #![feature(maybe_uninit_uninit_array)]
 #![feature(const_maybe_uninit_zeroed)]
@@ -24,12 +22,15 @@ extern crate fallible_collections;
 extern crate num_enum;
 extern crate static_assertions;
 
+extern crate multiboot2;
+
 use core::arch::asm;
 use core::ptr;
 
 use fallible_collections::FallibleVec;
 
 use memory::PagingProperties;
+use multiboot2::BootInformation;
 use utils::logging;
 
 use crate::process::TaskPriority;
@@ -37,17 +38,16 @@ use crate::process::TaskPriority;
 #[cfg(any(not(target_arch = "x86")))]
 compile_error!("Operation system is suitable for x86 CPU only");
 
-
+mod drivers;
 #[allow(dead_code)]
 mod file_system;
 #[allow(dead_code)]
 mod interrupts;
 #[allow(dead_code)]
 mod memory;
+mod process;
 #[allow(dead_code)]
 mod utils;
-mod drivers;
-mod process;
 
 #[cfg(not(test))]
 #[panic_handler]
@@ -69,8 +69,18 @@ pub unsafe extern "C" fn main() {
 }
 
 #[no_mangle]
-pub extern "C" fn _start() {
+pub extern "C" fn start(magic: u32, lp_header: *const u32) {
     logging::init();
+
+    if magic != multiboot2::MAGIC {
+        log!("Invalid grub2 magic");
+        return;
+    }
+
+    let mbi_ptr = lp_header.cast();
+    let mbi = unsafe {
+        BootInformation::load(mbi_ptr).expect("Failed to load mbi")
+    };
     log!("Working");
 }
 
@@ -83,15 +93,14 @@ pub fn rust_main(properties: &PagingProperties) {
     interrupts::init();
     let gdt = unsafe { properties.gdt().as_mut() };
     memory::enable_task_switching(gdt);
-    let thread_1 = process::new_task(task1, ptr::null_mut(), TaskPriority::HIGH);
+    let thread_1 =
+        process::new_task(task1, ptr::null_mut(), TaskPriority::HIGH);
     let thread_2 = process::new_task(task2, ptr::null_mut(), TaskPriority::LOW);
     process::submit_task(thread_1);
     process::submit_task(thread_2);
     process::run();
 }
 
-pub fn multi_boot_main(magic: u32, lp_header: *const u32) {
-}
 fn task1(_context: *mut ()) {
     log!("task 1 started");
     loop {
@@ -106,4 +115,82 @@ fn task2(_context: *mut ()) {
         process::sleep(400);
         log!("task 2 awaken");
     }
+}
+
+pub(self) fn print_memory_map(mbi: &BootInformation) -> anyhow::Result<()> {
+    let memmap = mbi
+        .memory_map_tag()
+        .ok_or("Should have memory map")
+        .map_err(anyhow::Error::msg)?;
+    log!("Memory Map:");
+    memmap.memory_areas().iter().for_each(|e| {
+        println!(
+            "  0x{:010x} - 0x{:010x} ({:.3} MiB {:?})",
+            e.start_address(),
+            e.end_address(),
+            e.size() as f32 / 1024.0 / 1024.0,
+            e.typ()
+        );
+    });
+    lon!();
+    Ok(())
+}
+
+pub(self) fn print_elf_info(mbi: &BootInformation) -> anyhow::Result<()> {
+    let sections_iter = mbi
+        .elf_sections()
+        .ok_or("Should have elf sections")
+        .map_err(anyhow::Error::msg)?;
+    println!("ELF sections:");
+    for s in sections_iter {
+        let typ = format!("{:?}", s.section_type());
+        let flags = format!("{:?}", s.flags());
+        let name = s.name().map_err(anyhow::Error::msg)?;
+        println!(
+            "  {:<13} {:<17} {:<22} 0x{:010x} 0x{:010x} {:>5.2} MiB align={}",
+            name,
+            typ,
+            flags,
+            s.start_address(),
+            s.end_address(),
+            s.size() as f32 / 1024.0,
+            s.addralign(),
+        );
+    }
+    println!();
+    Ok(())
+}
+
+pub(self) fn print_module_info(mbi: &BootInformation) -> anyhow::Result<()> {
+    let modules = mbi.module_tags().collect::<Vec<_>>();
+    if modules.len() != 1 {
+        Err(anyhow::Error::msg("Should have exactly one boot module"))?
+    }
+    let module = modules.first().unwrap();
+    let module_cmdline = module.cmdline().map_err(anyhow::Error::msg)?;
+    println!("Modules:");
+    println!(
+        "  0x{:010x} - 0x{:010x} ({} B, cmdline='{}')",
+        module.start_address(),
+        module.end_address(),
+        module.module_size(),
+        module_cmdline
+    );
+    println!(" grub cfg passed as boot module:");
+    let grup_cfg_ptr = module.start_address() as *const u32 as *const u8;
+    let grub_cfg =
+        unsafe { core::slice::from_raw_parts(grup_cfg_ptr, module.module_size() as usize) };
+
+    // In the GRUB bootflow case, we pass the config as module with it. This is
+    // not done for the chainloaded case.
+    if let Ok(str) = core::str::from_utf8(grub_cfg) {
+        println!("=== file begin ===");
+        for line in str.lines() {
+            println!("    > {line}");
+        }
+        println!("=== file end ===");
+        println!();
+    }
+
+    Ok(())
 }
